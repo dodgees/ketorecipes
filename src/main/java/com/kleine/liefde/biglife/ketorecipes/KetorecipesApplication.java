73afb7e55e0736694893136f215946ef729613c1
E. Dodge <dodgees2@gmail.com>
2018-03-03 21:23:14 -0600

    Initial commit

---
+++ src/main/java/com/kleine/liefde/biglife/ketorecipes/KetorecipesApplication.java
@@ -0,0 +1,12 @@
+package com.kleine.liefde.biglife.ketorecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetorecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetorecipesApplication.class, args);
+	}
+}
