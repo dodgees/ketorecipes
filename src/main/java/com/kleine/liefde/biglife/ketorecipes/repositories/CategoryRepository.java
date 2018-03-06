7d732ad046f169506375bb1057af14fac830def2
E. Dodge <dodgees2@gmail.com>
2018-03-05 19:57:04 -0600

    Adding data.sql and jpa query methods.

--- src/main/java/com/kleine/liefde/biglife/ketorecipes/repositories/CategoryRepository.java
+++ src/main/java/com/kleine/liefde/biglife/ketorecipes/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package com.kleine.liefde.biglife.ketorecipes.repositories;
 import com.kleine.liefde.biglife.ketorecipes.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Erik on 3/4/2018.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByCategoryName(String categoryName);
 }
--- src/main/java/com/kleine/liefde/biglife/ketorecipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/kleine/liefde/biglife/ketorecipes/repositories/UnitOfMeasureRepository.java
@@ -3,8 +3,12 @@ package com.kleine.liefde.biglife.ketorecipes.repositories;
 import com.kleine.liefde.biglife.ketorecipes.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Erik on 3/4/2018.
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
