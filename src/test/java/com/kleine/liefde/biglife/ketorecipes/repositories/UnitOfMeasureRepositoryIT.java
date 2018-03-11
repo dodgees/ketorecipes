a27c43477bdddfdafc56f53c1c657e5a9e0a869f
E. Dodge <dodgees2@gmail.com>
2018-03-11 14:12:25 -0500

    Adding tests with MockMVC for in IndexControllerTest and new integration  tests in UnitOfMeasureRepositoryIT.

---
+++ src/test/java/com/kleine/liefde/biglife/ketorecipes/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,46 @@
+package com.kleine.liefde.biglife.ketorecipes.repositories;
+
+import com.kleine.liefde.biglife.ketorecipes.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Erik on 3/11/2018.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+    }
+
+}
